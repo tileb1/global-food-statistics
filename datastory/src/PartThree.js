983f752dbde396c39de0547054147fa110b3509d
Tim <tim.lebailly@gmail.com>
2019-12-20 15:49:23 +0100

    more content

--- datastory/src/PartThree.js
+++ datastory/src/PartThree.js
@@ -1,47 +1,59 @@
+import ImportExportPlot from './ImportExportPlot';
 import React, { useState, useEffect } from 'react';
 import BarPlot from './BarPlot';
+import { interpolateBlues } from 'd3';
 
-function PartThree() {
+function PartTwo() {
+  return (<div className="part">
+    <h2>Who are the suspects?</h2>
+    <p>We have to normalize by import export blabablbala…. TRANSITION</p>
+    <p>Looking at the bar plot for the top exporters, the USA spike above others countries by far. They export more than twice as much as the next country on the list. Regarding the importers, there isn’t a country that imports substantially more than the others. It is worth noticing that the largest exporters are semantically different from the importers. Large exporters seem to be countries with a large land area, a large economy and moderate climate. On the other hand, large importers are countries with a large economy, and either a small land area with a high population density or a very dry climate. This makes sense since food production requires a lot of land area and countries with a high population density don’t have enough land area to subsist their nutrition needs.</p>
+    <PlotImportsExports/>
+    <p>Despite its conciseness, the import-export plot conveys a lot of information. It illustrates trading quantities with regards to food between pairs of countries as well as the top traded items of each countries. Check it out by hovering over a country! The USA is a great example. Not only have we seen that it is the top exporter, it is also the country that exports to the most countries. You can also toggle the arcs from exports to imports on the upper right of the plot.</p>
+    <ImportExportPlot/>
+    <p>Looking at the general trends on the plot, it is worth noting that countries seem to be clustered by their geographical position. This is indeed what we would expect from a capitalist market as it reduces the shipping costs of the merchandise. Another little detail that is also worth noticing is that African countries don’t seem to trade together too much. At first, it might seem weird but we have to keep in mind that these countries already have a hard time with food as most of them are food deficit countries and have to import from other continents. As such, all the internal production is used by the locals and there is no incentive to trade with others. South-America seems to follow a similar trend although not as considerable.</p>
+  </div>);
+}
 
+function PlotImportsExports() {
   const [data, setData] = useState(null);
   useEffect(() => {
     Promise.all([
-      fetch("sector_emissions.json"),
-      fetch("sector_emissions.json")
+      fetch("top_exporters.json"),
+      fetch("top_importers.json")
     ]).then((r) => Promise.all(r.map(e => e.json()))).then(([em1, em2]) => {
       setData({
-        emissions: em1,
-        crap: em2
+        exporters: em1,
+        importers: em2
       })
     });
   }, [])
 
-
-  const width = 350;
+  const width = 300;
   const height = 300;
   const color = "blue";
 
   return (<div className="part">
-    <h2>PART III: Something something</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat feugiat dapibus. Maecenas urna ipsum, scelerisque in congue scelerisque, finibus vel lorem. Etiam enim neque, fermentum sit amet nisl in, venenatis condimentum sapien. Maecenas tincidunt, neque a fringilla hendrerit, urna felis dapibus magna, sit amet efficitur enim elit nec nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque mattis fringilla justo eget scelerisque. Fusce finibus rhoncus dui, sit amet sagittis orci commodo eget. Vivamus pulvinar consequat nibh, ac pulvinar nisi imperdiet vitae. Aenean sed eros eu augue efficitur consequat blandit et sapien. Donec suscipit non dolor sit amet dictum. Phasellus facilisis purus et mi hendrerit, eu fringilla lacus placerat. Morbi hendrerit hendrerit lorem et consequat. Pellentesque non nulla felis.</p>
     <svg viewBox="0 0 800 500" xmlns="http://www.w3.org/2000/svg">
       {data && (<>
         <BarPlot
           width={width}
           height={height}
-          color={color}
-          data={data.emissions}
-          xLabel="Some"
-          xLabelShift={150}
-          yLabel="Shit"
+          color={interpolateBlues(1)}
+          data={data.exporters}
+          // xLabel="Some"
+          xLabelShift={500}
+          yLabel="Export value [Billion $]"
           rotateXVals={true}
-          transform="translate(30, 00)"
+          transform="translate(30, 0)"
         />
         <BarPlot
           width={width}
           height={height}
-          color="#a48"
-          data={data.crap}
+          color={interpolateBlues(1)}
+          data={data.importers}
+          rotateXVals={true}
+          yLabel="Import value [Billion $]"
           transform="translate(450, 0)"
         />
       </>)}
@@ -49,4 +61,4 @@ function PartThree() {
   </div>);
 }
 
-export default PartThree;
+export default PartTwo;
--- datastory/src/PartTwo.js
+++ datastory/src/PartTwo.js
@@ -1,11 +1,64 @@
-import React from 'react';
-import ImportExportPlot from './ImportExportPlot';
+// import React from 'react';
+// import ImportExportPlot from './ImportExportPlot';
+import React, { useState, useEffect } from 'react';
+import { interpolateBlues } from 'd3';
+import BarPlot from './BarPlot';
 
 function PartTwo() {
   return (<div className="part">
-    <h2>PART II: Crime scene investigation</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat feugiat dapibus. Maecenas urna ipsum, scelerisque in congue scelerisque, finibus vel lorem. Etiam enim neque, fermentum sit amet nisl in, venenatis condimentum sapien. Maecenas tincidunt, neque a fringilla hendrerit, urna felis dapibus magna, sit amet efficitur enim elit nec nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque mattis fringilla justo eget scelerisque. Fusce finibus rhoncus dui, sit amet sagittis orci commodo eget. Vivamus pulvinar consequat nibh, ac pulvinar nisi imperdiet vitae. Aenean sed eros eu augue efficitur consequat blandit et sapien. Donec suscipit non dolor sit amet dictum. Phasellus facilisis purus et mi hendrerit, eu fringilla lacus placerat. Morbi hendrerit hendrerit lorem et consequat. Pellentesque non nulla felis.</p>
-    <ImportExportPlot/>
+    <h2>Crime scene investigation</h2>
+    <p>As we have now established that a felony has been (or is being) committed, we aim to further investigate the crime scene. Particularly, from a nutritional point of view, we will examine what are the potential weapons (i.e. food types) used by the perpetrators (i.e. humanity) that have inflicted or are inflicting most damage to the planet and their level of implication.</p>
+    <p>Before delving into the various food types, we’re interested in actually seeing how much responsibilities the agricultural sector bears in terms of green house gas emissions. Below we plot the CO2-equivalent emissions by sector.</p>
+    <PlotEmissions/>
+    <p>We can observe that the agricultural sector’s emissions comes in fourth place. However, it is worth noting that the emissions attributed to the agricultural sector originate solely from the following: enteric fermentation, manure management, rice cultivation, synthetic fertilisers, manure applied to soils, manure left on pasture, crop residues and burning-crop residues. Therefore, even though the contribution of the agricultural sector to CO2 emissions seems to be undermined by those of Energy and Transport, the emissions that are truly caused by agriculture would be of a greater value than the one plotted above. This is due to the fact that a sizeable portion of CO2 emissions in both the Energy and Transport sectors are actually because of agriculture since agricultural activity induces the consumption of energy but also because food is hugely imported and exported across the world (we explore this point further later on).</p>
+    <p>Now that curiosity’s gotten the better of us, we look into what are the most and least devastating food types in terms of CO2 emitted. As we all know a vegetarian or a vegan person (or are one ourselves), you probably already heard that the killer food is meat. In order to verify this statement we plot both the proportion of total production that each food type represents and the proportion of total agricultural CO2 emissions that each food type is responsible for. Food types are divided between the following main raw foods: meats, milks and rice/cereals. We plot both below.</p>
+    <p>Unfortunately for all the meat lovers out there and to the delight of all vegetarians and vegans (for the sake of winning arguments), the striking observation we can make from the above plots is that meat is indeed extremely bad for the environment (compared to other foods). Even though it only represents roughly 7% of food production, it is responsible for over half of the CO2 equivalent emissions cause by agriculture. So you better start considering meat-less options more frequently for your meals. Although meat is bad for the environment, its production quantity is negligible in comparison to rice or cereals. This is mostly due to the fact that meat is generally more expensive and hence not as accessible to everyone such as its alternatives. One could potentially presume that our food production processes are simply inefficient in terms of greenhouse gas emissions. To explore this in more detail, we plot below the evolution of kilograms of CO2 equivalent emissions per kilogram produced of each food type.</p>
+    <p>In a more positive light, we can see that the efficiency of food production in terms of green house gas emissions has actually improved over time generally across all food types (including meat).  So although agricultural CO2 emissions are increasing due to rising population levels, we are making progress in terms of emission intensities.</p>
+    {/* <ImportExportPlot/> */}
+  </div>);
+}
+
+function PlotEmissions() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    Promise.all([
+      fetch("sector_emissions.json"),
+      fetch("sector_emissions.json")
+    ]).then((r) => Promise.all(r.map(e => e.json()))).then(([em1, em2]) => {
+      setData({
+        emissions: em1,
+        crap: em2
+      })
+    });
+  }, [])
+
+  const width = 750;
+  const height = 300;
+  const color = "blue";
+
+  return (<div className="part">
+    <svg viewBox="0 0 800 500" xmlns="http://www.w3.org/2000/svg">
+      {data && (<>
+        <BarPlot
+          width={width}
+          height={height}
+          color={interpolateBlues(1)}
+          data={data.emissions}
+          // xLabel="Some"
+          xLabelShift={150}
+          yLabel="CO2 [GTonnes]"
+          rotateXVals={true}
+          transform="translate(30, 00)"
+        />
+        {/* <BarPlot
+          width={width}
+          height={height}
+          color="#a48"
+          data={data.crap}
+          transform="translate(450, 0)"
+        /> */}
+      </>)}
+    </svg>
   </div>);
 }
 
